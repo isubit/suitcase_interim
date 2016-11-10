9d584242c601411712f6689335463dae3bddd68b
calsjp <calsjp@iastate.edu>
2016-11-10 12:22:23 -0600

    Basic quick links implemented. Current links are news, events, contact

--- template.php
+++ template.php
@@ -83,16 +83,6 @@ function suitcase_interim_preprocess_region(&$vars) {
     if (module_exists('taxonomy')) {
       $vars['categories'] = taxonomy_get_tree(1);
     }
-    $vars['site_name_level_2'] = variable_get('site_name');
-    $vars['site_name_level_3'] = variable_get('site_slogan');
-
-    // Levels to show
-    $vars['levels_to_show'] = variable_get('suitcase_interim_config_levels_to_show');
-
-    // The type of header that we need to output, default to show all
-    $vars['suitcase_interim_config_header_type'] = variable_get('suitcase_interim_config_header_type', 1);
-
-
   } 
   elseif ($vars['region'] == 'secondary_menu') {
     $theme = alpha_get_theme();
