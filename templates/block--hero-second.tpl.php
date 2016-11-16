54089596bdcb82753d64afd69ec50dcbe72a7153
Nick Booher <njbooher@gmail.com>
2016-11-16 11:21:04 -0600

    LUGG-876 Keep alpha class

--- templates/block--hero-second.tpl.php
+++ templates/block--hero-second.tpl.php
@@ -3,7 +3,7 @@
     <img src="<?php print $figure_image_url; ?>" alt="<?php print $figure_image_alt; ?>" />
     <?php if (!empty($figcaption_link_url)): ?>
       <div class="container-12">
-        <figcaption class="grid-12">
+        <figcaption class="grid-12 alpha">
           <a href="<?php print $figcaption_link_url; ?>"><?php print $figcaption_link_text; ?></a>
         </figcaption>
       </div>
