4a253c39c7d632d5b3dff02b4b561798c3a53775
Nick Booher <njbooher@gmail.com>
2017-12-28 09:57:31 -0600

    INTERIM-142 Use https

--- templates/default_region_content_blocks/footer_fourth.tpl.php
+++ templates/default_region_content_blocks/footer_fourth.tpl.php
@@ -1,10 +1,10 @@
 <p>Copyright &copy; <?php (module_exists('token_filter')) ? print('[current-date:custom:Y]') : print date('Y'); ?> <br /> Iowa State University of Science and Technology. All rights reserved.</p>
 <ul class="menu">
   <li class="first leaf">
-    <a href="http://www.policy.iastate.edu/policy/discrimination">Non-discrimination Policy</a>
+    <a href="https://www.policy.iastate.edu/policy/discrimination">Non-discrimination Policy</a>
   </li>
   <li class="leaf">
-    <a href="http://www.policy.iastate.edu/electronicprivacy">Privacy Policy</a>
+    <a href="https://www.policy.iastate.edu/electronicprivacy">Privacy Policy</a>
   </li>
   <li class="last leaf">
     <a href="https://www.digitalaccess.iastate.edu">Digital Access &amp; Accessibility</a>
