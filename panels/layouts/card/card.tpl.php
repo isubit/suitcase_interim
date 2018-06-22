c007a6ade2f58a625f51d24255f40286a765ad82
Carney, Sarah A <sacarney@iastate.edu>
2018-06-22 15:49:44 -0500

    INTERIM-169 Add new Card layout to panels

---
+++ panels/layouts/card/card.tpl.php
@@ -0,0 +1,27 @@
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="card <?php print $classes;?> clearfix">
+
+  <?php if (!empty($content['card_header'])): ?>
+    <div class="card-header">
+      <?php print $content['card_header']; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (!empty($content['card_image'])): ?>
+    <div class="card-image">
+      <?php print $content['card_image']; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (!empty($content['card_body'])): ?>
+    <div class="card-body">
+      <?php print $content['card_body']; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (!empty($content['card_footer'])): ?>
+    <div class="card-footer">
+      <?php print $content['card_footer']; ?>
+    </div>
+  <?php endif; ?>
+
+</<?php print $layout_wrapper ?>>
